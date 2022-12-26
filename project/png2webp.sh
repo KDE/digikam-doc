9dfa2106d5be7b1b0a739b68200c5b3ed7ebe864
Gilles Caulier <caulier.gilles@gmail.com>
2022-12-26 09:23:21 +0100

    script to convert PNG files to WEBP

---
+++ project/png2webp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ # ============================================================
+ #
+ # This file is a part of digiKam project
+ # http://www.digikam.org
+ #
+ # Date        : 2022-12-26
+ # Description : batch convert PNg to Webp using ImageMagick
+ #
+ # SPDX-FileCopyrightText: 2021-2022 by Gilles Caulier <caulier dot gilles at googlemail dot com>
+ #
+ # SPDX-License-Identifier: BSD-3-Clause
+ #
+ # ============================================================ */
+
+for f in *.png; do convert "$f"  $(basename "$f" | cut -d. -f1).webp ; done
